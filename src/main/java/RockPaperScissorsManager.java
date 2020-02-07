e4d7125e2fb33fc9f88886d3a8149590f0de4c6e
Liam Semanyk <liedsem26@gmail.com>
2020-02-06 21:32:50 -0500

    completed determine winner logic

--- src/main/java/RockPaperScissorsManager.java
+++ src/main/java/RockPaperScissorsManager.java
@@ -1,18 +1,17 @@
+import java.sql.SQLOutput;
+
 public class RockPaperScissorsManager {
 
     public static void play(Person playerOne, Person playerTwo){
         boolean gameOver = false;
 
         while(!gameOver){
-
-            playerOne.play();
-            playerTwo.play();
-
-            System.out.println(playerOne.getName() + " : " + playerOne.getCurrentMove());
-            System.out.println(playerTwo.getName() + " : " +  playerTwo.getCurrentMove());
-
-            if(determineWinner(playerOne,playerTwo) != null){
-                gameOver = false;
+            Person winner = determineWinner(playerOne,playerTwo);
+            if(winner == null){
+                System.out.println("tie");
+            }else{
+                System.out.println(winner.getName() + " is the winner!");
+                gameOver = true;
             }
 
         }
@@ -20,7 +19,52 @@ public class RockPaperScissorsManager {
     }
 
     private static Person determineWinner(Person PlayerOne, Person PlayerTwo){
-        return null;
+        Person winner = null;
+        PlayerOne.play();
+        String playerOneMove = PlayerOne.getCurrentMove();
+        PlayerTwo.play();
+        String playerTwoMove = PlayerTwo.getCurrentMove();
+
+        System.out.println(PlayerOne.getName() + " : " + PlayerOne.getCurrentMove());
+        System.out.println(PlayerTwo.getName() + " : " +  PlayerTwo.getCurrentMove());
+
+        //rock > scissors
+        if(playerOneMove.equalsIgnoreCase("rock")
+        && playerTwoMove.equalsIgnoreCase("scissors")){
+            winner = PlayerOne;
+        }
+
+        if(playerTwoMove.equalsIgnoreCase("rock")
+                && playerOneMove.equalsIgnoreCase("scissors")){
+            winner = PlayerTwo;
+        }
+
+        //scissors > paper
+        if(playerOneMove.equalsIgnoreCase("scissors")
+                && playerTwoMove.equalsIgnoreCase("paper")){
+            winner = PlayerOne;
+        }
+
+        if(playerTwoMove.equalsIgnoreCase("scissors")
+                && playerOneMove.equalsIgnoreCase("paper")){
+            winner = PlayerTwo;
+        }
+
+        //paper > rock
+        if(playerOneMove.equalsIgnoreCase("paper")
+                && playerTwoMove.equalsIgnoreCase("rock")){
+            winner = PlayerOne;
+        }
+
+        if(playerTwoMove.equalsIgnoreCase("paper")
+                && playerOneMove.equalsIgnoreCase("rock")){
+            winner = PlayerTwo;
+        }
+        if(playerOneMove.equalsIgnoreCase(playerTwoMove)){
+            winner = null;
+        }
+
+        return winner;
     }
 
 
